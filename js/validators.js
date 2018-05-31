bf71ce3f884f28110af405fd3a93a0d0a4a6472c
Aman Chauhan <amanc2610@gmail.com>
2018-05-31 22:35:00 +0530

    modified and fixed workshop add form

---
+++ js/validators.js
@@ -0,0 +1,67 @@
+(function ($) {
+  Drupal.behaviors.fossee_stats = {
+    attach: function (context, settings) {
+
+      if (typeof Drupal.settings.fossee_stats.numvidlink !== 'undefined')
+      {
+      	var numvidlink = Drupal.settings.fossee_stats.numvidlink;
+    		for(let i = 0; i < numvidlink; i++)
+    		{
+          let videoid = "";
+          if(i!=numvidlink-1)
+      		 videoid = "#edit-videolink-fieldset-"+i.toString()+"-videolink-link--"+(numvidlink-i).toString();
+          else 
+           videoid = "#edit-videolink-fieldset-"+i.toString()+"-videolink-link";
+
+          $(videoid).change(
+      	 	function(){ 
+      	 				let link = $(this).val(); 
+                console.log(i.toString());
+      	 				if( link.indexOf("http://") && link.indexOf("https://")) 
+      	 					{ $("#validate_url_"+i.toString()).css("visibility", "visible"); }
+      	 				else 
+      	 					$("#validate_url_"+i.toString()).css("visibility", "hidden");
+      	 			   } 
+     	    );
+          if(i!=numvidlink-1)
+          {
+            let link = $(videoid).val();
+            if( link.indexOf("http://") && link.indexOf("https://")) 
+                $("#validate_url_"+i.toString()).css("visibility", "visible"); 
+            else 
+                $("#validate_url_"+i.toString()).css("visibility", "hidden");  
+          }	
+    		}
+      } 
+
+       $("#edit-event-link").change(
+       		function(){ 
+       					var link = $(this).val(); 
+       					if( link.indexOf("http://") && link.indexOf("https://")) 
+       						$("#validate_url").css("visibility", "visible"); 
+       					else 
+                  $("#validate_url").css("visibility", "hidden");
+       				} 
+       );
+
+       $("#edit-end-date-datepicker-popup-0").change(
+       		function() { 
+       					if( $(this).val() < $("#edit-start-date-datepicker-popup-0").val()) 
+       						$("#validate_date").css("visibility","visible"); 
+       					else
+                 $("#validate_date").css("visibility","hidden"); 
+       				 }
+      	);
+
+       $("#edit-start-date-datepicker-popup-0").change(
+          function() { 
+                if( $("#edit-end-date-datepicker-popup-0").val()!="" && $(this).val() > $("#edit-end-date-datepicker-popup-0").val()) 
+                  $("#validate_date").css("visibility","visible"); 
+                else 
+                  $("#validate_date").css("visibility","hidden"); 
+               }
+        );
+
+    }
+  };
+}(jQuery));
